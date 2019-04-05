5d13c982a33d35d0f80aa5538048ca2134a55c53
Jialin Ma <marlin-@gmx.cn>
2019-04-05 02:21:12 -0400

    Create skeleton

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React {
+    constructor() {
+        super();
+        this.state = {
+            ok: true
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <p>hello</p>
+            </div>
+        )
+    }
+
+
+}
